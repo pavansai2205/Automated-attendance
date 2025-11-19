e274109724007f0e3a5c2d0c2b7b06c600b5c938
ANJAIAH <kaleelsai772@gmail.com>
2025-11-19 21:07:26 +0000

    Try fixing this error: `Runtime Error: Error: useEffect is not defined.

--- src/components/timetable-page.tsx
+++ src/components/timetable-page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useMemo, useState, useTransition } from 'react';
+import { useMemo, useState, useTransition, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
